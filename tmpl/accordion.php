4355970333af9f0ee5a76ff00212b1e393083c52
Sophist <github@sodalis.co.uk>
2014-05-12 12:24:12 +0100

    Improve generated HTML ...
    ... to provide first and last classes in HTML rather than using JS.

--- tmpl/accordion.php
+++ tmpl/accordion.php
@@ -7,11 +7,10 @@
 * More info at http://www.pixelpointcreative.com
 * Last Updated: 2/18/14
 */
-
 // No direct access.
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
-jimport( 'joomla.html.parameter' );
+jimport('joomla.html.parameter');
 JHtml::stylesheet('modules/'.$module->module.'/tmpl/css/accordion.css');
 
 // Load custom.css if it exists
@@ -24,81 +23,94 @@ if (is_file($file))
 $document =& JFactory::getDocument();
 include  "css" . DS . 'styles-accordion.php';
 $document->addStyleDeclaration( $style );
-include "js/accordion.js.php";
+include "js" . DS . "accordion.js.php";
+
+if(isset($menus) && count($menus)){
 ?>
+	<ul class='accordion-menu text-<?php echo $textAlign;?> button-<?php echo $bulletAlign;?>' id='accordion_menu_<?php echo $module->id;?>'>
+<?php
+	$countUlOpened = 1;
+	$level = 1;
+	$menu = $app->getMenu();
+	$active = ($menu->getActive()) ? $menu->getActive() : $menu->getDefault();
+	$path = $active->tree;
 
-	<?php if(isset($menus) && count($menus)){
-		$countUlOpened = 0;
-		$level = 1;
-		$menu		= $app->getMenu();
-		$active = ($menu->getActive()) ? $menu->getActive() : $menu->getDefault();
-		$path		= $active->tree;
-
-		for($i = 0; $i < count($menus); $i++){
-			if($i == 0){?>
-			     <ul class='accordion-menu accordion-menu-<?php echo $textAlign;?>' id='accordion_menu_<?php echo $module->id;?>'>
-			<?php	$countUlOpened++;
-			}
-			$class = "";
-			if(in_array($menus[$i]->id, $path)){
-				$class.= "class='current ";
-				$class.= "opened ";
-				$class .= "'";
-			}
-
-			$li = "<li ".$class.">";
-			$li .= "<div class='item-wrapper' >";
-			if($showBullet == "true"){
-				$divMenuButton = "<div class='menu-button menu-button-".$bulletAlign."' >";
-				if($i < count($menus)-1 && $menus[$i+1]->level > $menus[$i]->level){
-					$divMenuButton.="<img class='menuicon' alt='' src='".$bulletImage."'/>";
+	for($i = 0; $i < count($menus); $i++){
+		if($i == 0){$countUlOpened++;
+		}
+		$classes = array();
+		if(in_array($menus[$i]->id, $path)){
+			$classes[] = "current";
+			$classes[] = "opened";
+		}
+		if (($i == 0) || ($i > 0 && $menus[$i-1]->level < $menus[$i]->level)) {
+			$classes[] = "first";
+		}
+		// Final menu item or next menu item is a higher-level
+		if (($i == count($menus)-1) or ($i < count($menus)-1 && $menus[$i+1]->level < $menus[$i]->level)) {
+			$classes[] = "last";
+		}
+		// Next menuitem is a sub-menu - so skip forward until next non-sub-menu is same or higher level
+		if($i < count($menus)-1 && $menus[$i+1]->level > $menus[$i]->level){
+			for($j = $i+1; $j < count($menus) && ($menus[$j]->level != $menus[$i]->level); $j++) {
+				if($j == count($menus)-1 || $menus[$j]->level < $menus[$i]->level) {
+					$classes[] = "last";
+					break;
 				}
-				$divMenuButton .= "</div>";
-				$li.=$divMenuButton;
 			}
-			$style = "";
-			$target = "";
-			//echo $menus[$i]->browserNav . '<br/>';
-			switch ($menus[$i]->browserNav) :
+		}
+		$class = "";
+		if (!empty($classes)) {
+			$class = " class='" . implode(' ',$classes) . "'";
+		}
 
-				case 1:
-					$target=" target='_blank' ";
-					break;
-				case 2:
-					$target = " on-click=\"window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;\"";
-					break;
-			endswitch;
-			$icon_menu = ($menus[$i]->menu_image != '')?'<img src='.JURI::base().$menus[$i]->menu_image.' alt="menu icon" />':'';
-			$divLink = "<div class='menu-link' ".$style."><a ".$target." href='".$menus[$i]->flink."'>".$icon_menu.$menus[$i]->title."</a></div>";
-			$li.=$divLink;
-			$li.= "</div>";
-			echo $li;
-			if($i < count($menus)-1  && $menus[$i+1]->level > $menus[$i]->level ){
-				echo "<div class='ul-wrapper'><ul>";
-				$countUlOpened++;
-				$level++;
-
+		$li = "	<li".$class.">\r\n";
+		$li .= "		<div class='item-wrapper'>\r\n";
+		if($showBullet == "true"){
+			$divMenuButton = "			<div class='menu-button'>";
+			if($i < count($menus)-1 && $menus[$i+1]->level > $menus[$i]->level){
+				$divMenuButton.="<img class='menuicon' alt='' src='".$bulletImage."'/>";
 			}
-			if($i < count($menus)-1 && $menus[$i+1]->level < $menus[$i]->level ){
-				echo "</li></ul></div></li>";
+			$divMenuButton .= "</div>\r\n";
+			$li.=$divMenuButton;
+		}
+		$target = "";
+		switch ($menus[$i]->browserNav) :
+			case 1:
+				$target=" target='_blank'";
+				break;
+			case 2:
+				$target = " on-click=\"window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;\"";
+				break;
+		endswitch;
+		$icon_menu = ($menus[$i]->menu_image != '')?'<img src='.JURI::base(true).DS.$menus[$i]->menu_image.' alt="menu icon" />':'';
+		$divLink = "			<div class='menu-link'><a".$target." href='".$menus[$i]->flink."'>".$icon_menu.$menus[$i]->title."</a></div>\r\n";
+		$li.=$divLink;
+		$li.= "		</div>\r\n";
+		echo $li;
+
+		if ($i < count($menus)-1 && $menus[$i+1]->level > $menus[$i]->level) {
+			echo "	<div class='ul-wrapper'><ul>\r\n";
+			$countUlOpened++;
+			$level++;
+		}
+		if ($i < count($menus)-1 && $menus[$i+1]->level < $menus[$i]->level) {
+			echo "		</li>\r\n";
+			for($j = 0; $j < $menus[$i]->level - $menus[$i+1]->level; $j++) {
+				echo "	</ul></div></li>\r\n";
 				$countUlOpened--;
 				$level--;
-				for($j = 1; $j < $menus[$i]->level - $menus[$i+1]->level; $j++){
-					echo "</ul></div></li>";
-					$countUlOpened--;
-					$level--;
-				}
-			}
-			if( ($i < count($menus)-1 && $menus[$i+1]->level == $menus[$i]->level) || $i == count($menus)-1){
-				echo "</li>";
 			}
 		}
-		for($i=0; $i< $countUlOpened - 1; $i++){
-			echo "</ul></div>";
+		if ($i < count($menus)-1 && $menus[$i+1]->level == $menus[$i]->level) {
+			echo "	</li>\r\n";
 		}
-		echo "</ul>";
 	}
-
+	for ($i=0; $i < $countUlOpened - 1; $i++){
+		echo "	</li></ul></div>\r\n";
+	}
+	if ($countUlOpened > 1) {
+		echo "	</li></ul>\r\n";
+	}
+}
 ?>
-
-
